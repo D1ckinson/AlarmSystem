5779b759f9c7f79bf91b5f5f7fa793b3e6cca7fc
D1ckinson <123maxik321@mail.ru>
2024-03-26 23:25:41 +0300

    Add siren script

--- Assets/Scripts/AlarmZoneTrigger.cs
+++ Assets/Scripts/AlarmZoneTrigger.cs
@@ -1,64 +1,30 @@
-using System.Collections;
 using UnityEngine;
 
 public class AlarmZoneTrigger : MonoBehaviour
 {
-    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private Siren _siren;
 
-    private float _maxVolume = 100;
-    private float _volumeUpStep = 0.01f;
-    private float _delay = 0.2f;
-    private bool _hasThief;
-
-    private void Start()
-    {
-        _audioSource.volume = 0;
-        StartCoroutine(PlayAlarm());
-    }
+    private bool _isSyrenPlay;
 
     private void Update()
     {
-        if (_hasThief && _audioSource.isPlaying == false)
-            _audioSource.Play();
-        else if (_audioSource.volume <= 0 && _audioSource.isPlaying)
-            _audioSource.Stop();
+        //if (_isSyrenPlay && _siren.isPlaying == false)
+        //    _siren.Play();
+        //else if (_siren.volume <= 0 && _siren.isPlaying)
+        //    _siren.Stop();
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<Thief>())
-            _hasThief = true;
+            _siren.Play();
+        //_isSyrenPlay = true;
     }
 
     private void OnTriggerExit(Collider other)
     {
         if (other.GetComponent<Thief>())
-            _hasThief = false;
-    }
-
-    private IEnumerator PlayAlarm()
-    {
-        WaitForSeconds wait = new(_delay);
-
-        while (true)
-        {
-            while (_hasThief)
-            {
-                if (_audioSource.volume < _maxVolume)
-                {
-                    _audioSource.volume += _volumeUpStep;
-                }
-
-                yield return wait;
-            }
-
-            while (_audioSource.volume > 0)
-            {
-                _audioSource.volume -= _volumeUpStep;
-                yield return wait;
-            }
-
-            yield return null;
-        }
+            _siren.Stop();
+        //_isSyrenPlay = false;
     }
 }
---
+++ Assets/Scripts/Siren.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Siren : MonoBehaviour
+{
+    [SerializeField] private AudioSource _audioSource;
+
+    private float _maxVolume = 100;
+    private float _volumeUpStep = 0.01f;
+    private float _delay = 0.2f;
+
+    private bool _isPlaying;
+
+    private void Start() =>
+        _audioSource.volume = 0;
+
+    public void Play()
+    {
+        _isPlaying = true;
+
+        StartCoroutine(PlayAlarm());
+    }
+
+    public void Stop()
+    {
+        _isPlaying = false;
+
+        StartCoroutine(StopAlarm());
+    }
+
+    private IEnumerator PlayAlarm()
+    {
+        WaitForSeconds wait = new(_delay);
+
+        _audioSource.Play();
+
+        while (_isPlaying)
+        {
+            if (_audioSource.volume < _maxVolume)
+            {
+                _audioSource.volume += _volumeUpStep;
+            }
+
+            yield return wait;
+        }
+    }
+
+    private IEnumerator StopAlarm()
+    {
+        WaitForSeconds wait = new(_delay);
+
+        while (_audioSource.volume > 0)
+        {
+            _audioSource.volume -= _volumeUpStep;
+
+            yield return wait;
+        }
+
+        _audioSource.Stop();
+    }
+}
